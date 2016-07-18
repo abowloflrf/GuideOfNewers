034db526884a8f3ccc397427c5219cbdd2a506ad
Jason Lei - Acer <abowloflrf@outlook.com>
2016-07-19 03:00:20 +0800

    推到重做

--- js/index.js
+++ js/index.js
@@ -1,38 +1,15 @@
-function EasyPeasyParallax() {
-	scrollPos = $(this).scrollTop();
-	$('#banner').css({
-		'background-position' : '50% ' + (-scrollPos/4)+"px"
-	});
-	$('#bannertext').css({
-		'margin-top': (scrollPos/4)+"px",
-		'opacity': 1-(scrollPos/250)
-	});
-}
-
-$(document).ready(function(){
-	$(window).scroll(function() {
-		EasyPeasyParallax()
-	});
-});
-
-
 
 var toggleHeaderFloating = function() {
 			// Floating Header
 			if ( $(window).scrollTop() > 80 ) {
-				$( '.nav' ).addClass( 'floating' );
+				$( '.navbar' ).addClass( 'navbar-floating' );
+				$( '#logo' ).addClass( 'logo-floating' );
+				$('.navbar-right').addClass('navbar-right-floating');
 			} else {
-				$( '.nav' ).removeClass( 'floating' );
+				$( '.navbar' ).removeClass( 'navbar-floating' );
+				$( '#logo' ).removeClass( 'logo-floating' );
+				$('.navbar-right').removeClass('navbar-right-floating');
 			};
 		};
-var toggleSidebarFixed = function(){
-			if ( $(window).scrollTop() > 700 ) {
-				$( '.side-bar' ).addClass( 'fixed' );
-			} else {
-				$( '.side-bar' ).removeClass( 'fixed' );
-			};
-
-}
 
 		$(window).on( 'scroll', toggleHeaderFloating );
-		$(window).on( 'scroll', toggleSidebarFixed );
